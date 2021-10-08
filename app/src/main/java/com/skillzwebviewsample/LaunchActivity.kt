e4ebf4a1245a8c067ab70d6e0239cf6d9c112eee
EAdemov <e.ademov@casualino.com>
2021-10-08 17:45:24 +0300

    Initial commit

---
+++ app/src/main/java/com/skillzwebviewsample/LaunchActivity.kt
@@ -0,0 +1,25 @@
+package com.skillzwebviewsample
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.skillz.Skillz
+import com.skillz.SkillzActivity
+
+/**
+ * Created by enisademov on 8.10.21.
+ */
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.btnStart).setOnClickListener {
+            Skillz.launch(this)
+        }
+    }
+}
